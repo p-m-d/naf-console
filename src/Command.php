fa853d2b804ae731e324fc53066e826946b270d9
Paul Webster <paul@webprogression.co.nz>
2015-07-16 09:29:16 +1200

    Ch-ch-ch changes

--- src/Command.php
+++ src/Command.php
@@ -2,6 +2,27 @@
 
 namespace Naf\Console;
 
-class Command {}
+abstract class Command {
+
+	public $input;
+
+	public $output;
+
+	public $name;
+
+	public function __construct($input = null, $output = null, $name = null) {
+		$this->input = $input ?: Console::input();
+		$this->output = $output ?: Console::output();
+		if (!isset($this->name)) {
+			if (empty($name)) {
+				$classPath = explode('\\', get_class($this));
+				$name = preg_replace('/Command$/', '', end($classPath));
+			}
+			$this->name = $name;
+		}
+	}
+
+	abstract public function __invoke();
+}
 
 ?>
--- src/Console.php
+++ src/Console.php
@@ -2,10 +2,59 @@
 
 namespace Naf\Console;
 
+use Naf\App;
+use Infiltrate\FilterableStaticTrait;
+
 class Console {
 
+	use FilterableStaticTrait;
+
+	public static $classes = [
+		'input' => 'Naf\Console\Input',
+		'output' => 'Naf\Console\Output'
+	];
+
+	public static function bootstrap() {
+		$namespace = 'Naf\\Console';
+		$provides = [
+			'command' => ['Command', 'Console']
+		];
+		$params = compact('namespace', 'provides');
+		return static::_filter(__FUNCTION__, $params, function($self, $params){
+			extract($params);
+			App::provide($namespace, $provides);
+		});
+	}
+
 	public static function dispatch() {
-		var_dump(__METHOD__);
+		global $argv;
+		$args = array_slice($argv, 1);
+		if (empty($args)) {
+			$message = 'No command specified';
+			throw new \Exception($message);
+		}
+		$command = str_replace(' ', '', ucwords(str_replace('_', ' ', array_shift($args))));
+		$console = App::locate($command . 'Command', 'command');
+		$invoke = new $console(static::input(), static::output(), $command);
+		$invoke();
+	}
+
+	public static function input() {
+		$class = static::$classes['input'];
+		$params = compact('class');
+		return static::_filter(__FUNCTION__, $params, function($self, $params){
+			extract($params);
+			return new $class();
+		});
+	}
+
+	public static function output() {
+		$class = static::$classes['output'];
+		$params = compact('class');
+		return static::_filter(__FUNCTION__, $params, function($self, $params){
+			extract($params);
+			return new $class();
+		});
 	}
 }
 
